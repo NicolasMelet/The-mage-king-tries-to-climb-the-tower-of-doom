7e951a3363a904667f2eb8612f10b70ca946829d
Manuel Tomé <105550975+ManuelR-T@users.noreply.github.com>
2024-11-09 16:48:44 +0100

    feat: player sprite and animation

--- nouveau-projet-de-jeu/scripts/player.gd
+++ nouveau-projet-de-jeu/scripts/player.gd
@@ -3,16 +3,22 @@ extends CharacterBody2D
 
 @export var SPEED = 300.0
 @export var JUMP_VELOCITY = -600.0
+@onready var animation_player: AnimationPlayer = $Sprite2D/AnimationPlayer
+
+var last_facing_direction := Vector2(0, 1)
+var anim_direction : String
+
+func _ready() -> void:
+	pass
+	animation_player.play("idle_left")
 
 func player():
 	pass
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
@@ -21,5 +27,16 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
 	move_and_slide()
+	#if velocity:
+		#last_facing_direction = velocity.normalized()
+	#
+	#if last_facing_direction.x > 0:
+		#anim_direction = "right"
+	#elif last_facing_direction.x < 0:
+		#anim_direction = "left"
+	#
+	#if velocity.x != 0:
+		#animation_player.play("walk_" + anim_direction)
+	#else:
+		#animation_player.play("idle_" + anim_direction)
