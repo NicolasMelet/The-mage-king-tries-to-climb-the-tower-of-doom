39fb04bdb25dde892f75b7479f0d686203fc6678
NicolasMelet <nicolas.melet@epitech.eu>
2024-11-09 12:09:32 +0100

    feat: Base village form is complete

--- nouveau-projet-de-jeu/scripts/camera_2d.gd
+++ nouveau-projet-de-jeu/scripts/camera_2d.gd
@@ -1,6 +1,7 @@
 extends Camera2D
 
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -8,11 +9,11 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var players = get_tree().get_nodes_in_group("Players")
-	
-	for player in players:
-		print(player.position)
-		print(limit_right)
+	#var players = get_tree().get_nodes_in_group("Players")
+	#
+	#for player in players:
+		#print(player.position)
+		#print(limit_right)
 		#if player.position.x < position.x:
 			#position.x -= limit_right
 	pass
