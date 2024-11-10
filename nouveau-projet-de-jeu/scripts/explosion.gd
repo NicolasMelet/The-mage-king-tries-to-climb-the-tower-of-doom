f2e01c2f8f55c89a1ac5512f4eb9b77015287c96
NicolasMelet <nicolas.melet@epitech.eu>
2024-11-10 01:21:12 +0100

    level one complete

--- nouveau-projet-de-jeu/scripts/explosion.gd
+++ nouveau-projet-de-jeu/scripts/explosion.gd
@@ -1,13 +1,17 @@
 extends Area2D
 
-@export var EXPLOSION_STRENGTH = 3000
+@export var EXPLOSION_STRENGTH = [400, 750, 1000]
+@export var EXPLOSION_STRENGTH_ITEMS = [700, 1000, 2000]
 var MAX_TIME = 0.2
 var CURRENT_TIME = 0
+var size = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
+func setSize(newSize):
+	size = newSize
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -19,9 +23,9 @@ func _process(delta: float) -> void:
 func _on_body_entered(body: Node2D) -> void:
 	if body is RigidBody2D:
 		var direction = global_position.direction_to(body.global_position)
-		body.apply_impulse(EXPLOSION_STRENGTH * direction)
+		body.apply_impulse(EXPLOSION_STRENGTH_ITEMS[size] * direction)
 	if (body.is_in_group("Player")):
 		@warning_ignore("integer_division")
-		body.velocity = global_position.direction_to(body.global_position) * EXPLOSION_STRENGTH / (EXPLOSION_STRENGTH / 800)
+		body.velocity = global_position.direction_to(body.global_position) * EXPLOSION_STRENGTH[size]
 		@warning_ignore("integer_division")
-		body.explosion((global_position.direction_to(body.global_position) * EXPLOSION_STRENGTH / (EXPLOSION_STRENGTH / 800)).x)
+		body.explosion((global_position.direction_to(body.global_position) * EXPLOSION_STRENGTH[size]).x)
--- nouveau-projet-de-jeu/scripts/player.gd
+++ nouveau-projet-de-jeu/scripts/player.gd
@@ -1,16 +1,19 @@
 extends CharacterBody2D
 
 
-@export var SPEED = 300.0
+@export var SPEED = 100.0
 @export var JUMP_VELOCITY = -300.0
 @export var push_force = 80.0
 @onready var animation_player: AnimationPlayer = $Sprite2D/AnimationPlayer
 const FIRE_BALL = preload("res://scenes/fireBall.tscn")
-@onready var AIR_FRICTION = 20
+@onready var AIR_FRICTION = 5
 var energyExplosion = 0
 
 var last_facing_direction := Vector2(0, 1)
 var anim_direction : String
+var countTimeCharge = 0
+@export var MAX_CHARGE = 0.7
+@export var MID_CHARGE = 0.2
 
 func _ready() -> void:
 	animation_player.play("idle_left")
@@ -30,22 +33,33 @@ func _physics_process(delta: float) -> void:
 		#velocity.y = JUMP_VELOCITY
 	
 	# Handle jump.
-	if Input.is_action_just_pressed("fireball"):
+	if Input.is_action_pressed("fireball"):
+		countTimeCharge += delta
+
+	if countTimeCharge > 0 and (Input.is_action_just_released("fireball") or countTimeCharge >= MAX_CHARGE):
 		var fireball = FIRE_BALL.instantiate()
+		if countTimeCharge >= MAX_CHARGE:
+			fireball.setSize(2)
+		elif countTimeCharge >= MID_CHARGE:
+			fireball.setSize(1)
+		else:
+			fireball.setSize(0)
+		countTimeCharge = 0
 		var futur_pos = global_position
 		futur_pos.x += 8
 		fireball.set_global_position(futur_pos)
 		fireball.rotation = global_position.direction_to(get_global_mouse_position()).angle()
+		fireball.global_position += global_position.direction_to(get_global_mouse_position()) * 50
 		owner.add_child(fireball)
 
 	var direction := Input.get_axis("left", "right") if not is_on_floor() else 0.0
-	print(abs(velocity.x + direction * SPEED))
 	if direction:
 		velocity.x = direction * SPEED + energyExplosion
 	elif velocity.x != 0:
 		velocity.x = move_toward(velocity.x, 0, AIR_FRICTION)
 	
 	energyExplosion = move_toward(energyExplosion, 0, AIR_FRICTION)
+	
 	
 	move_and_slide()
 	for i in get_slide_collision_count():
